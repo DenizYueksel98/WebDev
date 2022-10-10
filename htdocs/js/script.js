309043e8c198fc768439142db8a87530915716e7
DenizYueksel98 <115476742+DenizYueksel98@users.noreply.github.com>
2022-10-10 16:39:36 +0200

    Add files via upload

---
+++ htdocs/js/script.js
@@ -0,0 +1,9 @@
+console.log("ok")
+
+let mydata={
+}
+
+console.log(mydata)
+
+/*The console.log() method outputs a message to the web console. The message may be a single string (with optional substitution values), or it may be any one or more JavaScript objects.
+Note: This feature is available in Web Workers*/
