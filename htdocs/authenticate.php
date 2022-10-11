8938c2664591a86aa500c9837c4b5115edd80f2e
DenizYueksel98 <115476742+DenizYueksel98@users.noreply.github.com>
2022-10-11 13:47:08 +0200

    Sign-Up+Login hinzugefügt
    Registrierung und Anmeldung hinzugefügt. Anmeldung per Link auf authenticate.php

---
+++ htdocs/authenticate.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+$DATABASE_HOST = 'mariadb';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = 'wwi2021a';
+$DATABASE_NAME = 'wwi2021a';
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+if ( mysqli_connect_errno() ) {
+	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+
+$id = $con->real_escape_string($_POST['usernameLogin']);
+$password= $con->real_escape_string($_POST['passwordLogin']);
+if ( !isset($_POST['usernameLogin'], $_POST['passwordLogin']) ) {
+	exit('Bitte gib sowohl den Nutzernamen als auch dein Passwort ein!');
+}
+if ($stmt = $con->prepare('SELECT ID, Passwort FROM userdata WHERE Benutzername = ?')) {
+	$stmt->bind_param('s', $_POST['usernameLogin']);
+	$stmt->execute();
+	$stmt->store_result();
+    if ($stmt->num_rows > 0) {
+        $stmt->bind_result($id, $password);
+        $stmt->fetch();
+        if ($_POST['passwordLogin']== $password) {
+            session_regenerate_id();
+            $_SESSION['loggedin'] = TRUE;
+            $_SESSION['name'] = $_POST['usernameLogin'];
+            $_SESSION['id'] = $id;
+            echo 'Welcome ' . $_SESSION['name'] . '!';
+        } else {
+            echo 'Passwort falsch!';
+        }
+    } else {echo 'Nutzername falsch!';
+    }
+
+	$stmt->close();
+}
+?>
--- htdocs/database.php
+++ htdocs/database.php
@@ -8,12 +8,13 @@ $mysqli = new mysqli("mariadb", $username, $password, $database);
 // Don't forget to properly escape your values before you send them to DB
 // to prevent SQL injection attacks.
 
-$field1 = $mysqli->real_escape_string($_POST['field1']);
-$field2 = $mysqli->real_escape_string($_POST['field2']);
-$field3 = $mysqli->real_escape_string($_POST['field3']);
+$field1 = $mysqli->real_escape_string($_POST['username']);
+$field2 = $mysqli->real_escape_string($_POST['password']);
+$field3 = $mysqli->real_escape_string($_POST['firstname']);
+$field4 = $mysqli->real_escape_string($_POST['lastname']);
 
-$query = "INSERT INTO testTable (ID, Name, Passwort)
-            VALUES ('{$field1}','{$field2}','{$field3}')";
+$query = "INSERT INTO userdata (Benutzername, Passwort, Vorname, Nachname)
+            VALUES ('{$field1}','{$field2}','{$field3}','{$field4}')";
 
 $mysqli->query($query);
 $mysqli->close();
