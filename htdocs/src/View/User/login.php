5efb5679ae5bf706f3c8b9364721a9ed709678de
Yueksel <Deniz.Yueksel@trumpf.com>
2022-10-27 11:45:49 +0200

    authenticate renamed login
    authenticate renamed to login for better understanding :+1: Added JS, atm no function

--- htdocs/src/View/User/login.php
+++ htdocs/src/View/User/login.php
@@ -1,5 +1,7 @@
+<script src="../../../js/jQuery.min.js" type="text/javascript"></script>
+<script src="../../../js/create_user.js"></script>
 <h1>Falls du dich schon registriert hast, kannst du dich auch hier anmelden</h1>
-<form action="http://localhost:8080/src/Model/User/authenticate.php" method="post">
+<form id="login-form" method="post">
     <div class="input-container">
         <input type="text" name="usernameLogin" placeholder=" " />
         <label for="username" class="placeholder">Benutzername</label>
@@ -8,8 +10,7 @@
         <input type="password" name="passwordLogin" placeholder=" " />
         <label for="password" class="placeholder">Passwort</label>
     </div>
-
-    <button class="button" type="submit">
+    <button id="loginbtn" class="button" type="submit">
         Anmelden
     </button>
 </form>
