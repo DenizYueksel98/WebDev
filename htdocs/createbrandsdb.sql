356ff6fa8a1cbfa711f1bf1219fb69c1cbb396c4
SebastianMuenz1988 <sebastian.muenz.24@gmail.com>
2022-11-09 12:54:15 +0100

    Hoffe so passt es

--- htdocs/createbrandsdb.sql
+++ htdocs/createbrandsdb.sql
@@ -1,30 +1,18 @@
-USE test;
+USE `test`;
 
-CREATE TABLE `cars` (
-    `id` SMALLINT(3) NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(100) NOT NULL CHECK (Length (nachname) <60),
-    `b21` SMALLINT(4) NOT NULL CHECK (Length(`b21`)<=4),
-    `b22` SMALLINT(3) NOT NULL CHECK (Length(`b22`)<=3),
-    `j` CHAR(2) NOT NULL CHECK (Length(`j`)<=2),
-    `vier` CHAR(4) NOT NULL CHECK (Length(`vier`)<=4),
-    `d1` VARCHAR(50) NOT NULL CHECK (Length(`vier`)<=60),
-    `d21` VARCHAR(50) NOT NULL CHECK (Length(`vier`)<=3),
-    `d22` VARCHAR(50) NOT NULL CHECK (Length(`vier`)<=4),
-    `d23` VARCHAR(50) NOT NULL CHECK (Length(`vier`)<=60),
-    `zwei` VARCHAR(50) NOT NULL CHECK (Length(`vier`)<=60),
-    `fuenf1` VARCHAR(50) NOT NULL CHECK (Length(`vier`)<=60),
-    `fuenf2` VARCHAR(50) NOT NULL CHECK (Length(`vier`)<=60),
-    `v9` VARCHAR(50) NOT NULL CHECK (Length(`vier`)<=60),
-    `vierzehn` VARCHAR(50) NOT NULL CHECK (Length(`vier`)<=4),
-    `p3` VARCHAR(50) NOT NULL CHECK (`p3`="BENZIN" OR `p3`="WASSERSTOFF" OR `p3`="GAS" OR`p3`="ELEKTRO" OR`p3`="DIESEL");
-    `verbin` DECIMAL(5) NOT NULL CHECK (`verbin`between 0 and 300),
-    `verbin_unit` VARCHAR(5) NOT NULL CHECK (`verbin`between 0 and 300),
-    `verbau` DECIMAL(5) NOT NULL CHECK (`verbin`between 0 and 300),
-    `verbko` DECIMAL(5) NOT NULL CHECK (`verbko`between 0 and 300),
-    `co2kom` DECIMAL(5) NOT NULL CHECK (`co2kom`between 0 and 300),
-    `sehrs` DECIMAL(5) NOT NULL CHECK (`sehrs`between 0 and 300),
-    `schnell` DECIMAL(5) NOT NULL CHECK (`schnell`between 0 and 300),
-    `langsam` DECIMAL(5) NOT NULL CHECK (`langsam`between 0 and 300),
-    `co2komb` DECIMAL(5) NOT NULL CHECK (`co2komb`between 0 and 300),
-    PRIMARY KEY (id)
-) 
+CREATE TABLE `marke` (
+    `brand` VARCHAR(30) NOT NULL ,
+    `gruendort` VARCHAR(30) NOT NULL,
+    `gruendjahr` VARCHAR(30) NOT NULL,
+    `mitarbeiter` VARCHAR(30) NOT NULL,
+    `jahresumsatz` VARCHAR(30) NOT NULL,
+    PRIMARY KEY(`brand`)
+);
+
+INSERT INTO `marke` VALUE
+("MERCEDES", "Stuttgart, Detschland", 1926, 173000, 109600000000),
+("CADILLAC", "Detroit, Michigan, USA", 1909, 12300, 17400000000),
+("BMW", "München", 1916, 118900, 111239000000),
+("KIA", "Stuttgart", 1926, 52000, 39800000000),
+("VOLKSWAGEN", "Stuttgart", 1926, 192000, 250200000000);
+
--- htdocs/createcardb.sql
+++ htdocs/createcardb.sql
@@ -1,12 +1,17 @@
-USE test;
+/* DROP DATABASE IF EXISTS test;
+
+CREATE DATABASE test;
+
+USE test; */
+
 
 CREATE TABLE `cars` (
     `id` SMALLINT(3) NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(100) NOT NULL CHECK (Length (nachname) <60),
+    `name` VARCHAR(100) NOT NULL CHECK (Length (`name`) <60),
     `b21` SMALLINT(4) NOT NULL CHECK (Length(`b21`)<=4),
     `b22` SMALLINT(3) NOT NULL CHECK (Length(`b22`)<=3),
-    `j` CHAR(2) NOT NULL CHECK (Length(`j`)<=2),
-    `vier` CHAR(4) NOT NULL CHECK (Length(`vier`)<=4),
+    `j` VARCHAR(2) NOT NULL CHECK (Length(`j`)<=2),
+    `vier` VARCHAR(4) NOT NULL CHECK (Length(`vier`)<=4),
     `d1` VARCHAR(50) NOT NULL CHECK (Length(`vier`)<=60),
     `d21` VARCHAR(50) NOT NULL CHECK (Length(`vier`)<=3),
     `d22` VARCHAR(50) NOT NULL CHECK (Length(`vier`)<=4),
@@ -16,15 +21,65 @@ CREATE TABLE `cars` (
     `fuenf2` VARCHAR(50) NOT NULL CHECK (Length(`vier`)<=60),
     `v9` VARCHAR(50) NOT NULL CHECK (Length(`vier`)<=60),
     `vierzehn` VARCHAR(50) NOT NULL CHECK (Length(`vier`)<=4),
-    `p3` VARCHAR(50) NOT NULL CHECK (`p3`="BENZIN" OR `p3`="WASSERSTOFF" OR `p3`="GAS" OR`p3`="ELEKTRO" OR`p3`="DIESEL");
+    `p3` VARCHAR(50) NOT NULL CHECK (`p3`="BENZIN" OR `p3`="WASSERSTOFF" OR `p3`="GAS" OR`p3`="ELEKTRO" OR`p3`="DIESEL"),
     `verbin` DECIMAL(5) NOT NULL CHECK (`verbin`between 0 and 300),
-    `verbin_unit` VARCHAR(5) NOT NULL CHECK (`verbin`between 0 and 300),
     `verbau` DECIMAL(5) NOT NULL CHECK (`verbin`between 0 and 300),
     `verbko` DECIMAL(5) NOT NULL CHECK (`verbko`between 0 and 300),
-    `co2kom` DECIMAL(5) NOT NULL CHECK (`co2kom`between 0 and 300),
+    `co2kom1` DECIMAL(5) NOT NULL CHECK (`co2kom1`between 0 and 300),
     `sehrs` DECIMAL(5) NOT NULL CHECK (`sehrs`between 0 and 300),
     `schnell` DECIMAL(5) NOT NULL CHECK (`schnell`between 0 and 300),
     `langsam` DECIMAL(5) NOT NULL CHECK (`langsam`between 0 and 300),
-    `co2komb` DECIMAL(5) NOT NULL CHECK (`co2komb`between 0 and 300),
-    PRIMARY KEY (id)
-) 
+    `co2kom2` DECIMAL(5) NOT NULL CHECK (`co2kom2`between 0 and 300),
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`d1`) REFERENCES `marke`(`brand`)
+);
+
+
+INSERT INTO `cars`
+(`name`,
+`b21`,
+`b22`,
+`j`,
+`vier`,
+`d1`,
+`d21`,
+`d22`,
+`d23`,
+`zwei`,
+`fuenf1`,
+`fuenf2`,
+`v9`,
+`vierzehn`,
+`p3`,
+`sehrs`,
+`schnell`,
+`langsam`,
+`co2kom1`,
+`verbin`,
+`verbau`,
+`verbko`,
+`co2kom2`) 
+VALUE 
+("BMW 123", 
+ 8781,
+ 187,
+ "FC",
+ "SFa",
+ "BMW",
+ "D21",
+ "8N6d",
+ "7IOKOR9V",
+ "5P5D5M5POJRWDFG",
+ "DRDFWLRDJFLSRJEKLKSF",
+ "LFKEJSLKJFELSF",
+ "F2JKFSEFJSLFJSF",
+ "KSLFKJELFJKSLKF",
+ "BENZIN",
+ 8.1,
+ 6.18,
+ 4.53,
+ 70.34,
+ 6.53,
+ 3.61,
+ 4.3,
+ 34.5);
